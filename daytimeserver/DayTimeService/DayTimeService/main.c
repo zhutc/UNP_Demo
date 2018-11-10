7f81b25b0d2f7056a2a09f1c75e942f41a151fa5
chuting <502353919@qq.com>
2018-11-10 10:25:40 +0800

    【高】add day time server

---
+++ daytimeserver/DayTimeService/DayTimeService/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DayTimeService
+//
+//  Created by zhutc on 2018/11/10.
+//  Copyright © 2018年 zhuoApp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

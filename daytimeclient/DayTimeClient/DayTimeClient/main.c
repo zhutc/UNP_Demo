ea3c23b27e89e8d44c95c8e2ee0b25344b6f247c
ChuTing <chuting@ChuTingdeMacBook-Pro.local>
2018-11-10 10:32:30 +0800

    【高】add day time client

---
+++ daytimeclient/DayTimeClient/DayTimeClient/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DayTimeClient
+//
+//  Created by ChuTing on 2018/11/10.
+//  Copyright © 2018年 Opple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
